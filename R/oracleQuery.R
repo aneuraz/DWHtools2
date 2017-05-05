2dccf371c6347d76e4d635acfc3b1117f683c5ef
Antoine Neuraz <antoine.neuraz@gmail.com>
2017-05-05 09:41:58 +0200

    convert results colnames to uppercase to be compatible with postgres results

--- R/oracleQuery.R
+++ R/oracleQuery.R
@@ -4,41 +4,42 @@
 #' @return the result of the query (data.frame)
 #' @export
 oracleQuery <- function(query, config = config, update = F, data = T) {
-  
-  
+
+
   result <- list()
   i = 1
   # set connection
   jdbcDriver <- RJDBC::JDBC(driverClass=config$driverClass, classPath=config$classPath)
   jdbcConnection <- RJDBC::dbConnect(jdbcDriver, config$connectPath, config$dbuser, config$dbpass)
-  
+
   # query
   if (update) {
     result <- RJDBC::dbSendUpdate(jdbcConnection, query)
   } else {
     res<- RJDBC::dbSendQuery(jdbcConnection, query)
     #res <- dbFetch(res)
-    
+
     if (data) {
       while(nrow(chunk <- DBI::dbFetch(res, n = 30000)) > 0 ) {
         result[[i]] <- chunk
         i <- i +1
       }
-      
+
       result<- plyr::rbind.fill(result)
-      
+
     }
 
     RJDBC::dbClearResult(res)
   }
-  
+
   # disconnection
   RJDBC::dbDisconnect(jdbcConnection)
-  
+
   if (data) {
+    names(result) <- toupper(names(result))
     return(result)
   }
-  
+
 }
-  
+
 
